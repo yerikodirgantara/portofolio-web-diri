9230ba775502c34558496c6e9de553de546190a4
verenyerikodirgantara <yerikodirgantara@gmail.com>
2025-04-17 14:42:01 +0700

    commit

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(".project-button").click(function () {
-    $(this).fadeOut(200).fadeIn(200); // Animasi memudar dan muncul kembali
-  });
-});
-var myModal = new bootstrap.Modal(document.getElementById('projectModal1'));
-document.querySelector('#projects .project-button').addEventListener('click', function () {
-  myModal.show();
-});
